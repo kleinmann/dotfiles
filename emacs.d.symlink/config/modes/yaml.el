ab221a7ae24da5a14657c24c9dd63191dc4198d3
Uwe Kleinmann <u.kleinmann@googlemail.com>
2012-01-31 18:36:27 +0100

    Removes my shot at an emacs.d. Adds emacs-prelude (for when I actually have to use emacs or come around to try it out some more).

--- emacs.d.symlink/config/modes/yaml.el
+++
@@ -1,4 +0,0 @@
-(defun yaml-mode-hook ()
-  (autoload 'yaml-mode "yaml-mode" nil t)
-  (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
-  (add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode)))
