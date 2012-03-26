0f67dfec17f8c787c83f8df4956dd62f487a3c83
Uwe Kleinmann <u.kleinmann@googlemail.com>
2012-03-26 17:56:59 +0200

    Configures hippie-expand and adds a magit binding.

--- emacs.d.symlink/kleinmann/bindings.el
+++ emacs.d.symlink/kleinmann/bindings.el
@@ -4,3 +4,4 @@
 (global-set-key (kbd "C-S-r") 'remember)
 ; I have mapped Command to Meta, but I still want to be able to quit with Command+q
 (global-set-key (kbd "M-q") 'save-buffers-kill-terminal)
+(global-set-key (kbd "C-c g") 'magit-status)
