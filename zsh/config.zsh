f49d509f1bbc6f7a26170fe62e09c3a105567c9f
Uwe Kleinmann <u.kleinmann@googlemail.com>
2012-07-12 00:57:06 +0200

    Fixes keybindings in zsh.

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -47,5 +47,30 @@ bindkey '^[[1;9A' beginning-of-line
 bindkey '^[[1;9B' end-of-line
 bindkey '^[[3~' delete-char
 
+bindkey -v
+
+bindkey "\e[1~" beginning-of-line
+bindkey "\e[4~" end-of-line
+bindkey "\e[5~" beginning-of-history
+bindkey "\e[6~" end-of-history
+bindkey "\e[3~" delete-char
+bindkey "\e[2~" quoted-insert
+bindkey "\e[5C" forward-word
+bindkey "\eOc" emacs-forward-word
+bindkey "\e[5D" backward-word
+bindkey "\eOd" emacs-backward-word
+bindkey "\e\e[C" forward-word
+bindkey "\e\e[D" backward-word
+bindkey "^H" backward-delete-word
+bindkey "\e[8~" end-of-line
+bindkey "\e[7~" beginning-of-line
+bindkey "\eOH" beginning-of-line
+bindkey "\eOF" end-of-line
+bindkey "\e[H" beginning-of-line
+bindkey "\e[F" end-of-line
+bindkey '^i' expand-or-complete-prefix
+bindkey '\e[3~' delete-char
+bindkey '^R' history-incremental-search-backward
+
 # Include local settings if there are any
 [ -f $HOME/.zshrc_local ] && source $HOME/.zshrc_local
