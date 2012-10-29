cd62632404ec508202b5124e4fe72796c91af753
Uwe Kleinmann <u.kleinmann@googlemail.com>
2012-10-29 19:47:04 +0100

    Make git nicer.

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,6 +1,7 @@
 [[ -x `which ruby` ]] && alias git=hub
-alias gs='git status'
+alias gs='git status -sb'
 alias gd='git diff'
 alias gp='git pull'
+alias git-amend='git commit --amend'
 alias gl="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias changelog="git log --pretty=format:'%an, %aD: %s%n' --reverse | grep -v Merge"
