31dda1755fed1781de2b064abf9a1e2bc8488024
Uwe Kleinmann <u.kleinmann@googlemail.com>
2012-12-01 17:36:55 +0100

    Additions to PATH.

--- system/paths.zsh
+++ system/paths.zsh
@@ -8,10 +8,12 @@ PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin
 export PATH=$HOME/bin:$PATH
 
 # add java bin
-export PATH=$PATH:/opt/java/bin
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_09.jdk/Contents/Home
+export PATH=$PATH:$JAVA_HOME/bin
 
 # add python bin
 export PATH=$PATH:/Library/Frameworks/Python.framework/Versions/2.6/bin
+export PATH=$PATH:/usr/local/share/python
 
 # haskell
 export PATH=$PATH:$HOME/.cabal/bin
@@ -21,6 +23,7 @@ export PATH=$PATH:/Applications/Network:/opt/local/bin:/opt/local/sbin:/usr/texb
 
 # npm
 export PATH=$PATH:$HOME/node_modules/.bin
+export PATH=$PATH:/usr/local/share/npm/bin
 
 # original $MANPATH
 # usr/share/man:/usr/local/share/man:/usr/X11/man
