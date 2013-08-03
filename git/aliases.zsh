a70a32acf4b995259144cdf3ed2b1582e2c8a844
Uwe Kleinmann <u.kleinmann@googlemail.com>
2013-08-03 14:01:14 +0200

    Switch git aliases and add one for standups.

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -7,5 +7,5 @@ alias gd='git diff'
 alias gp='git pull'
 alias gpr='git stash ; git pull --rebase ; git stash pop'
 alias git-amend='git commit --amend'
-alias gl="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias gl="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --all --decorate"
 alias changelog="git log --pretty=format:'%an, %aD: %s%n' --reverse | grep -v Merge"
