aa80e1e9b2546a35cb07353953b9292d9fa9b548
Uwe Kleinmann <u.kleinmann@googlemail.com>
2013-08-04 18:11:27 +0200

    Remove CDPATH, annoys me more than it helps.

--- system/env.zsh
+++ system/env.zsh
@@ -5,9 +5,6 @@ export SVN_EDITOR=$EDITOR
 export GIT_EDITOR=$EDITOR
 export PAGER=less
 
-# Use CDPATH, cd searches there automatically
-export CDPATH=.:$HOME
-
 # Load colors
 if [ -x /usr/bin/dircolors ]; then
 	eval "`dircolors -b`"
