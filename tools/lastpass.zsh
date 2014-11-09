597be629e509763e8ead541d4b71189978e090d3
Uwe Kleinmann <u.kleinmann@googlemail.com>
2014-11-09 23:10:13 +0100

    Add handy lpass cli aliases

---
+++ tools/lastpass.zsh
@@ -0,0 +1,3 @@
+alias lpga='lpass show --clip --password "Google Account"'
+alias lpgk='lpass show --clip --password "Google Apps kleinmann.org"'
+alias lpgc='lpass show --clip --password "Google Apps codekunst"'
