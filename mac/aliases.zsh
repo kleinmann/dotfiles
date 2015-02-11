a9cf911d4581512bfcee8c64319bfabb597e5d84
Uwe Kleinmann <u.kleinmann@googlemail.com>
2015-02-11 22:22:25 +0100

    Fix mac detection..

--- mac/aliases.zsh
+++ mac/aliases.zsh
@@ -1,11 +1,11 @@
 # Remove all items safely, to Trash (`brew install trash`).
-[ -x `which trash` ] && alias rm='trash'
+(( $+commands[trash] )) && alias rm='trash'
 
 # Case-insensitive pgrep that outputs full path.
-[ `uname` == "Darwin" ] && alias pgrep='pgrep -fli'
+[ `uname` = "Darwin" ] && alias pgrep='pgrep -fli'
 
 # Lock current session and proceed to the login screen.
-[ `uname` == "Darwin" ] && alias lock='/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend'
+[ `uname` = "Darwin" ] && alias lock='/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend'
 
 # Sniff network info.
-[ `uname` == "Darwin" ] && alias sniff="sudo ngrep -d 'en1' -t '^(GET|POST) ' 'tcp and port 80'"
+[ `uname` = "Darwin" ] && alias sniff="sudo ngrep -d 'en1' -t '^(GET|POST) ' 'tcp and port 80'"
