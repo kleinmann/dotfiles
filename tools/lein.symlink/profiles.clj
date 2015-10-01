5df3b1248e02c72297441669f3d8baf44ec07e7e
Uwe Kleinmann <u.kleinmann@googlemail.com>
2015-10-01 14:28:39 +0200

    Add leiningen configuration.

---
+++ tools/lein.symlink/profiles.clj
@@ -0,0 +1,8 @@
+{:user {:plugins [
+                  [lein-pprint "1.1.1"]
+                  [cider/cider-nrepl "0.9.1"]
+                  [refactor-nrepl "0.3.0-SNAPSHOT"]]
+        :dependencies [
+                       [slamhound "1.5.0"]
+                       [alembic "0.3.2"]
+                       [org.clojure/tools.nrepl "0.2.7"]]}}
