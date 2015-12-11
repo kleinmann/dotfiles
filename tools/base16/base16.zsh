ce69c4548a9da15cd56e70fd8aeaff7a5a5acae5
Uwe Kleinmann <uwe@kleinmann.org>
2015-12-11 13:36:47 +0100

    Change base16 theme and tmux theme.

--- tools/base16/base16.zsh
+++ tools/base16/base16.zsh
@@ -1,3 +1,3 @@
 # Base16 Shell
-BASE16_SHELL="$ZSH/tools/base16/base16-solarized.light.sh"
-[[ -s $BASE16_SHELL ]] && source $BASE16_SHELL
+BASE16_SHELL="$ZSH/tools/base16/base16-monokai-dark.sh"
+#[[ -s $BASE16_SHELL ]] && source $BASE16_SHELL
