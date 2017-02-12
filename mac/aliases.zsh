3c721cc751f862c573df22577455a63eea43bab6
Uwe Kleinmann <uwe@kleinmann.org>
2017-02-12 01:19:17 +0100

    Add alias to remove .DS_Store files.

--- mac/aliases.zsh
+++ mac/aliases.zsh
@@ -11,3 +11,5 @@
 [ `uname` = "Darwin" ] && alias sniff="sudo ngrep -d 'en1' -t '^(GET|POST) ' 'tcp and port 80'"
 
 [ `uname` = "Darwin" ] && alias pdfmerge="'/System/Library/Automator/Combine PDF Pages.action/Contents/Resources/join.py' -o"
+
+alias cleanup_dsstore="find . -name '*.DS_Store' -type f -ls -delete"
